e1f114cb173ab058278edad90f91e55bc7961ee3
JIAE LEE <60423068+dlwod1010@users.noreply.github.com>
2021-02-21 07:01:35 -0800

    Create a development example site-BIG
    Responsive
    Add research
    Add Google tools

---
+++ big/carts.php
@@ -0,0 +1,11 @@
+<?php include 'includes/header.php';?>
+    <p>shopping carts</p>
+  
+</section>
+
+<aside>
+    <h3>Link</h3>
+       
+</aside>
+
+<?php include 'includes/footer.php';?>
---
+++ big/map.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php';?>
+
+<div class='embed-container'>
+    <iframe 
+    src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.5050995528636!2d-122.32339218484763!3d47.616312079185285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906accc351c149%3A0xdc1a5c338dd4395c!2sSeattle%20Central%20College!5e0!3m2!1sen!2skr!4v1613828078869!5m2!1sen!2skr' width='600' height='450' style='border:0;' allowfullscreen='' loading='lazy'>
+    </iframe>
+</div>
+
+</section>
+
+<aside>
+    <h3>Seattle Central College</h3>
+        <address>
+        1701 Broadway<br/>
+        Seattle, WA 98122
+        </address>
+        <a href="tel:2069343800">206.934.3800</a>
+</aside>
+
+<?php include 'includes/footer.php';?>
