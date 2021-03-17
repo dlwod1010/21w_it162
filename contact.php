48e0f87ac370cfacc3ab4a5f5e0e474e18b3ac39
JIAE LEE <60423068+dlwod1010@users.noreply.github.com>
2021-03-17 13:08:17 +0900

    final project -Dondduk website
    create pages with .php files

---
+++ contact.php
@@ -0,0 +1,45 @@
+<?php include 'includes/header.php';?>
+<section class="contact-page">
+        
+        <div class="contact-container">
+            <div class="form-wrapper">
+                <h2 class="headline">Tell us how we do!</h2>
+                <form action="">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name..">
+            
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email-address" placeholder="dondduk@abc.com">
+                    
+                <label for="feedback">Give us your feedback!</label>
+                <textarea id="feedback" name="feedback" ></textarea>
+            
+                <input type="submit" value="submit" class="form-btn">
+                </form>
+            </div>
+            
+            <div class="info-wrapper">                
+                <div class="contact-info">
+                    <div class="hours">
+                        <p>[open]</p>  
+                        <p>Monday through Saturday</p>
+                        <p>[hours]</p>  
+                        <p><strong>lunch</strong> 11:00 am - 2:30 pm</p>
+                        <p><strong>dinner</strong> 4:30 pm - 10:00 pm</p>
+                    </div>
+                    
+                </div>
+            
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.755859198763!2d126.98383081474286!3d37.3956055798303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b5e45322b0fcd%3A0xb2d8bc78b18c14b1!2z64-I65qdKOuPiOuCmOyZgOudvCDrmp3rlLEp!5e0!3m2!1sko!2skr!4v1615645004994!5m2!1sko!2skr"  style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    <p class="tel">T. 031-423-4455</p>
+                    <p class="address">54, Botdeul-ro, Uiwang-si, Gyeonggi-do, Republic of Korea</p>
+                </div>
+            </div>
+        </div>
+    </section>  
+
+    <?php include 'includes/footer.php';?>
---
+++ order.php
@@ -0,0 +1,135 @@
+<?php include 'includes/header.php';?>
+<section class ="order-page">
+        <h2 class="headline">Online Ordering</h2>
+        <p class="headline">Pick up | Curbside pick up</p>
+    
+        <div class="order">
+            <div class="select-menu">
+                <img class ="select-img" src="images/badalgogi.jpg"  alt="Grilled pork" >
+                <h3>Grilled Samgyupsal</h3>
+                <p class="price">$19.99</p>
+                <p><i class="fas fa-thumbs-up"></i>  Highly recommended menu. Pork, Griiled Samgyupsal with rice, vegi and soup! Simple and classic is always the best!</p>
+                
+                <div class="select-qtt">
+                    <input type ="number" value="1" min="1" max="20">
+                    <button class ="select-add-btn" >Add</button>
+                    <button class="select-remove-btn">Remove</button>
+                </div>                    
+                <div class="select-total">
+                    <label>Total</label>
+                    <div class="select-total-value" id="select-value"></div>
+                </div>        
+            </div>            
+
+            <div class="select-menu">
+                <img class =" select-img" src="images/badalgogi2.jpg"  alt="spicy pork" >
+                <h3>Jeyukbokum</h3>
+                <p class="price">$19.99</p>
+                <p>Fried gochujang souce marinated pork comes with rice, vegi and soup! Spicy food lover? Should try!</p>
+                
+                <div class="select-qtt">
+                    <input type ="number" value="1" min="1" max="20">
+                    <button class ="select-add-btn" >Add</button>
+                    <button class="select-remove-btn">Remove</button>
+                </div>    
+                <div class="select-total">
+                    <label>Total</label>
+                    <div class="select-total-value" id="select-value"></div>
+                </div>        
+            </div>
+
+            <div class="select-menu">
+                <img class =" select-img" src="images/badalgogi3.jpg"  alt="Bulgogi" >
+                <h3>Bulgogi</h3>
+                <p class="price">$19.99</p>
+                <p><i class="fas fa-thumbs-up"></i>  Bulgogi comes with rice, vegi and soup! One of the best menu in our restaurant.</p>
+                
+                <div class="select-qtt">
+                    <input type ="number" value="1" min="1" max="20">
+                    <button class ="select-add-btn" >Add</button>
+                    <button class="select-remove-btn">Remove</button>
+                </div>    
+                <div class="select-total">
+                    <label>Total</label>
+                    <div class="select-total-value" id="select-value"></div>
+                </div>        
+            </div>
+
+            <div class="select-menu">
+                <img class =" select-img" src="images/badalgogi4.jpg" alt="Beef tartare" >
+                <h3>Beef Tartare Bibimbab</h3>
+                <p class="price">$23.99</p>
+                <p>Spicy souce beef tartare with rice, vegi and soup! You'd love this Special Bibimbab! </p>
+                
+                <div class="select-qtt">
+                    <input type ="number" value="1" min="1" max="20">
+                    <button class ="select-add-btn" >Add</button>
+                    <button class="select-remove-btn">Remove</button>
+                </div>    
+                <div class="select-total">
+                    <label>Total</label>
+                    <div class="select-total-value" id="select-value"></div>
+                </div>        
+            </div>
+            
+            <div class="select-menu">
+                <img class =" select-img" src="images/badalgogi.jpg"  alt="pork-samgyusal" >
+                <h3>Grilled Samgyupsal</h3>
+                <p class="price">$19.99</p>
+                <p><i class="fas fa-thumbs-up"></i>Highly recommended menu. Pork, Griiled Samgyupsal with rice, vegi and soup! Simple and classic is always the best!</p>
+                
+                <div class="select-qtt">
+                    <input type ="number" value="1" min="1" max="20">
+                    <button class ="select-add-btn" >Add</button>
+                    <button class="select-remove-btn">Remove</button>
+                </div>    
+                <div class="select-total">
+                    <label>Total</label>
+                    <div class="select-total-value" id="select-value"></div>
+                </div>        
+            </div>
+
+            <div class="select-menu">
+                <img class =" select-img" src="images/badalgogi.jpg"  alt="pork-samgyusal" >
+                <h3>Grilled Samgyupsal</h3>
+                <p class="price">$19.99</p>
+                <p><i class="fas fa-thumbs-up"></i>Highly recommended menu. Pork, Griiled Samgyupsal with rice, vegi and soup! Simple and classic is always the best!</p>
+                
+                <div class="select-qtt">
+                    <input type ="number" value="1" min="1" max="20">
+                    <button class ="select-add-btn" >Add</button>
+                    <button class="select-remove-btn">Remove</button>
+                </div>    
+                <div class="select-total">
+                    <label>Total</label>
+                    <div class="select-total-value" id="select-value"></div>
+                </div>        
+            </div>
+
+            <div class="select-menu">
+                <img class =" select-img" src="images/badalgogi.jpg"  alt="pork-samgyusal" >
+                <h3>Grilled Samgyupsal</h3>
+                <p class="price">$19.99</p>
+                <p><i class="fas fa-thumbs-up"></i>Highly recommended menu. Pork, Griiled Samgyupsal with rice, vegi and soup! Simple and classic is always the best!</p>
+                
+                <div class="select-qtt">
+                    <input type ="number" value="1" min="1" max="20">
+                    <button class ="select-add-btn" >Add</button>
+                    <button class="select-remove-btn">Remove</button>
+                </div>    
+                <div class="select-total">
+                    <label>Total</label>
+                    <div class="select-total-value" id="select-value"></div>
+                </div>        
+            </div>           
+
+        </div>
+
+        <div class ="checkout">
+            <button class="checkout-btn">checkout</button>
+        </div>
+         
+            
+    </section>
+
+    <?php include 'includes/footer.php';?>
