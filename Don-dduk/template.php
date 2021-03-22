25fadb1c4142c8df22a18720b4d1c74bb7e5b41f
JIAE LEE <60423068+dlwod1010@users.noreply.github.com>
2021-03-22 16:21:50 -0700

    Upload files - Dondduk
    Dondduk HTML files

---
+++ Don-dduk/template.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/5c31fb8743.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styles.css">
+        <title>A template</title>
+</head>
+<body>
+  <header >
+    <h2 class="pageID"></h2>
+    <div class="top-container">
+        <div class="top-logo">
+          <a href="template.php">
+          Template
+          </a>
+        </div>
+        <div class="sub-logo">Aliquam tincidunt mauris eu risus.</div>
+        <div class="top-nav">
+          <i class="fas fa-bars"></i>
+          <div class="nav">            
+            <a href="" class="delivery">delivery order</a>
+            <a href="about.php">about</a>
+            <a href="menu.php">menu</a>
+            <a href="contact.php">contact</a>
+            <a href="https://map.naver.com/v5/entry/place/1539489212?c=14136004.1448371,4494401.7337420,15,0,0,0,dh&placePath=%2Fbooking%3Fentry=plt" target="_blank" class="reservation">reservation</a>
+          </div>
+        </div>
+    </div>
+  </header>
+  
+  <section>
+    <div class="banner-wrapper">        
+        <div class="banner-info">
+            <p>[open]</p>  
+            <p>Monday through Saturday</p>
+            <p>[hours]</p>  
+            <p><strong>lunch</strong> 11:00 am - 2:30 pm</p>
+            <p><strong>dinner</strong> 4:30 pm - 10:00 pm</p>
+            <p><a href="https://www.instagram.com/godyks357/" class="insta-link"><i class="fas fa-hashtag"></i>Donddukinstagram</a></p>
+        </div>
+        <div class="banner-image">
+        <img src="http://lorempixel.com/400/200" >
+        </div>
+    </div>
+    
+    <div class="gallery-wrapper">
+      <div class="gallery">
+        <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
+        <img src="http://lorempixel.com/400/200" />
+      </div>              
+    </div>
+  </section>
+  <footer>
+    <p><small>&copy; 2021 by <a href="contactme.php" target="_blank">Jiae Lee</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+  </footer>
+</body>
+</html>
